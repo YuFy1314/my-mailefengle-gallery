767d5d068e22c9718e4de90b3caf711895b1970c
YuFy1314 <yufy_info@qq.com>
2018-12-07 10:13:45 +0800

    update bootstrap to the @3 version

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -1,6 +1,6 @@
 require('muse-ui/dist/muse-ui.css');
-require('../../..//node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('../../..//node_modules/bootstrap/dist/js/bootstrap.min.js');
+require('../../../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../../../node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('@/assets/less/reset.less');
 require('@/assets/less/common.less');
 require('@/assets/less/theme/theme-default.less');
