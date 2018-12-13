1d4f247b4111c2b0761d03e2bc47fab50a746182
YuFy1314 <yufy_info@qq.com>
2018-12-13 10:49:53 +0800

    封装了一个模态框组件Modal.vue, 其他组件可以直接公用插槽即可

---
+++ src/public/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="modal">
+        <mu-container>
+            <mu-dialog width="360" :open.sync="openSimple">
+                <slot name="header">
+                    <h3>{{ modal.title }}</h3>
+                </slot>
+                <slot name="content"></slot>
+                <slot name="footer">
+                    <mu-button slot="actions" flat color="default" @click="closeSimpleDialog">关闭</mu-button>
+                </slot>
+            </mu-dialog>
+        </mu-container>
+    </div>
+</template>
+<script>
+export default {
+    props: ['modalOptions'],
+    data() {
+        return {
+            openSimple: false
+        }
+    },
+    computed: {
+        modal() {
+            var modal = this.modalOptions || {};
+            var modalOptions = {
+                title: modal.title || '系统提示'
+            };
+            return modal;
+        }
+    },
+    methods: {
+        openSimpleDialog() {
+            this.openSimple = true;
+        },
+        closeSimpleDialog() {
+            this.openSimple = false;
+        }
+    }
+}
+
+</script>
+<style>
+
+
+</style>
